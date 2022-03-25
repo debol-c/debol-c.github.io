73792c443ccde8231e5f8002c2599329563e21b5
Kerod Gebremedhin <kerodgebremedhin@gmail.com>
2022-03-25 07:16:51 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -100,7 +100,7 @@
      * ------------------------------------------------------ */
     const ssFinalCountdown = function () {
 
-        const finalDate = '2022/03/21';
+        const finalDate = '2022/04/10';
 
         $('.counter').countdown(finalDate)
             .on('update.countdown finish.countdown', function (event) {
